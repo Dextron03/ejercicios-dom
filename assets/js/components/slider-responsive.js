5d30ffede9d35ea15fb85c9c6086059ef6bd3433
Dextron03 <brailyrs03@gmail.com>
2025-01-27 01:20:31 -0400

    Hice una funcion para pasar los slide a la derecha, para asi poder hacer que se pasen en automatico dentro de un setinterval

--- assets/js/components/slider-responsive.js
+++ assets/js/components/slider-responsive.js
@@ -1,31 +1,41 @@
-export const slider = () => {
-    const $nextBtn = document.querySelector(".next"),
-    $pevtBtn = document.querySelector(".prev"),
-    $slides = document.querySelectorAll(".slider-slide");
+const $nextBtn = document.querySelector(".next"),
+$pevtBtn = document.querySelector(".prev"),
+$slides = document.querySelectorAll(".slider-slide");
 
-    let i = 0; //Contador
+let i = 0,//Contador
+pasadorSlide = null;
+
+const nextSlide = () => {
+    $slides[i].classList.remove("active");
+    i++;
+
+    if(i == $slides.length) i = 0;
     
+    $slides[i].classList.add("active");
+}
+
+export const slider = () => {
+    pasadorSlide = setInterval(()=>{
+        nextSlide();
+    },5000);
+
     document.addEventListener("click", (e) => {
         if(e.target === $pevtBtn){
             e.preventDefault();
             $slides[i].classList.remove("active");
             i--;
             
-            if(i < 0){
-                i = $slides.length - 1;
-            }
+            if(i < 0) i = $slides.length - 1;
+
             $slides[i].classList.add("active");
+
+            clearInterval(pasadorSlide);
         }
 
         if(e.target === $nextBtn){
             e.preventDefault();
-            $slides[i].classList.remove("active");
-            i++;
-
-            if(i == $slides.length){
-                i = 0;
-            }
-            $slides[i].classList.add("active");
+            nextSlide();
+            clearInterval(pasadorSlide);
         }
     })
 }
